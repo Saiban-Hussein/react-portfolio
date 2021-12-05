a462f40ec740b492dc5b3fd2a5ee6225c2b80f00
Saiban Hussein <shhussein@m.marywood.edu>
2021-12-05 16:45:27 -0600

    Updates

---
+++ precache-manifest.811cc96b7d52a30224aaa23aada0e5eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c276e97b1c7c70e9e74bfd0c0ceabfc1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f529c944b828afae038d",
+    "url": "/react-portfolio/static/css/main.51888003.chunk.css"
+  },
+  {
+    "revision": "dcaf1fc9617e4c625de2",
+    "url": "/react-portfolio/static/js/2.5e1758e0.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "/react-portfolio/static/js/2.5e1758e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a05c1c123fe9808c7d3",
+    "url": "/react-portfolio/static/js/3.ceb9b950.chunk.js"
+  },
+  {
+    "revision": "f529c944b828afae038d",
+    "url": "/react-portfolio/static/js/main.4fee2048.chunk.js"
+  },
+  {
+    "revision": "e2b82bf176cd120c6edb",
+    "url": "/react-portfolio/static/js/runtime-main.00160076.js"
+  },
+  {
+    "revision": "a781efe2a501ba15a616e0dce6aa7c13",
+    "url": "/react-portfolio/static/media/Resume.a781efe2.pdf"
+  },
+  {
+    "revision": "483d855bf21d31379b4b323c347744cf",
+    "url": "/react-portfolio/static/media/Saiban.483d855b.png"
+  },
+  {
+    "revision": "95c19c42fe33327ceeff3f7c9ab83c06",
+    "url": "/react-portfolio/static/media/book-search-engine.95c19c42.jpg"
+  },
+  {
+    "revision": "9fb59c3df582932a5c562af552247268",
+    "url": "/react-portfolio/static/media/code-quize.9fb59c3d.png"
+  },
+  {
+    "revision": "04630e3d83b4b30906b933d496fa42e5",
+    "url": "/react-portfolio/static/media/note-taker-image.04630e3d.png"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/react-portfolio/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "35ced407a32c88709a42f7fdef5fb819",
+    "url": "/react-portfolio/static/media/screenshot.35ced407.png"
+  },
+  {
+    "revision": "ea6dc2d0d07d20db7c7f719857e8a56c",
+    "url": "/react-portfolio/static/media/weather-dashboard-image.ea6dc2d0.jpg"
+  }
+]);
